131ec5cd552b3ac9213fff1a505431f1bcb8b01c
rakib <phi.rakib@gmail.com>
2024-06-28 09:06:14 +0600

    added product, cart, success, cancel & checkout route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
+use App\Livewire\Cart;
+use App\Livewire\Checkout;
+use App\Livewire\ProductList;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', ProductList::class)->name('products.index');
+
+Route::get('/cart', Cart::class)->name('cart.index');
+
+Route::get('/success', function() {
+  return view('success');
+})->name('success');
+
+Route::get('/cancel', function() {
+  return view('cancel');
+})->name('cancel');
+
+Route::get('/checkout', Checkout::class)->name('checkout');
